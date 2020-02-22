6f3a6338e752ac0a5560b014aad53d76216e3ccc
Anna <annussaa@gmail.com>
2020-02-22 13:37:13 +0200

    init solution

---
+++ BookScanning/BookScanning/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BookScanning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
